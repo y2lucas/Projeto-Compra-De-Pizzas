f1f97216801f51f2489acb9eb792567709781f4a
Lucas Vasconcelos <164563464+y2lucas@users.noreply.github.com>
2026-05-18 20:53:17 -0300

    Projeto de Comprar pizzas no momento atual eu adicionei as imagems valor e descrições, ao invés de puxar de uma API estou puxando tudo pelo arquivo pizzas

---
+++ script.js
@@ -0,0 +1,21 @@
+const c = (el) => {
+    return document.querySelector(el);
+}
+const cs = (el) => {
+    return document.querySelectorAll(el)
+}
+
+pizzaJson.map((item, index) => {
+    let pizzaItem = c(".models .pizza-item").cloneNode(true);
+    pizzaItem.querySelector('.pizza-item--img img').src = item.img;
+    pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`;
+    pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
+    pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
+    pizzaItem.querySelector('a').addEventListener('click',(e)=>{
+        e.preventDefault();
+
+        
+    });
+
+    c('.pizza-area').append(pizzaItem);
+});
