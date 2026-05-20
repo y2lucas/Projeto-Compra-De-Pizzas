8576f8fb856f89e47e8a5431a0257c3743d80251
Lucas Vasconcelos <164563464+y2lucas@users.noreply.github.com>
2026-05-19 23:23:55 -0300

    Fiz poucas mundanças só fiz a animação do carrinhos de compras

--- script.js
+++ script.js
@@ -7,14 +7,20 @@ const cs = (el) => {
 
 pizzaJson.map((item, index) => {
     let pizzaItem = c(".models .pizza-item").cloneNode(true);
+
+    pizzaItem.setAttribute('data-key', index)
     pizzaItem.querySelector('.pizza-item--img img').src = item.img;
     pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`;
     pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
     pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
-    pizzaItem.querySelector('a').addEventListener('click',(e)=>{
+    pizzaItem.querySelector('a').addEventListener('click', (e) => {
         e.preventDefault();
+        c(".pizzaWindowArea").style.opacity = 0;
+        c(".pizzaWindowArea").style.display = "flex";
+        setTimeout(() => {
+            c(".pizzaWindowArea").style.opacity = 1;
 
-        
+        }, 200)
     });
 
     c('.pizza-area').append(pizzaItem);
